d0d8bab86e8ee4ec8f9f8f949a93e156bf721bea
Kazuki Yoneyama <y.kazu_0916@icloud.com>
2022-09-30 23:55:50 +1000

    connect to mongo DB

--- crud_app/server.js
+++ crud_app/server.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const bodyparser = require("body-parser");
 const path = require('path');
 
+const connectDB = require('./server/database/connection');
+
 const app = express();
 
 dotenv.config({path:'config.env'})
@@ -11,6 +13,9 @@ const PORT = process.env.PORT||8080
 
 //log requests
 app.use(morgan('tiny'));
+
+// mongodb connection
+connectDB();
 
 //parse request to body-parser
 app.use(bodyparser.urlencoded({ extended:true}))
@@ -24,9 +29,8 @@ app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
 app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
 app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
+// load routers
+app.use('/', require('./server/routes/router'))
 
-app.get('/', (req, res)=>{
-  res.render('index');
-})
 
 app.listen(PORT,()=>{console.log(`Server is running on http://localhost:${PORT}`)});
