6702273e0fb5eccf6edc84d512d99ad10c9972b3
Kazuki Yoneyama <y.kazu_0916@icloud.com>
2022-10-02 01:21:09 +1000

    crud application is completed

--- crud_app/server/controller/controller.js
+++ crud_app/server/controller/controller.js
@@ -20,7 +20,8 @@ exports.create=(req,res)=>{
   user
     .save(user)
     .then(data => {
-      res.send(data)
+      // res.send(data)
+      res.redirect('/add-user')
     })
     .catch(err=>{
       res.status(500).send({
@@ -31,13 +32,32 @@ exports.create=(req,res)=>{
 
 // retrive and return all users/ retrive and return a single user
 exports.find=(req, res)=>{
-  Userdb.find()
+
+  if(req.query.id){
+    const id =req.query.id;
+
+    Userdb.findById(id)
+      .then(data=>{
+        if(!data){
+          res.status(404).send({message: "Not found user with id"+id})
+        }else{
+          res.send(data)
+        }
+      })
+      .catch(err=>{
+        res.status(500).send({message:"Error retrieving user id "+id})
+      })
+  }else{
+    Userdb.find()
   .then(user=>{
     res.send(user)
   })
   .catch(err=>{
     res.status(500).send({message:err.message||"Error Occured while retriving user information"})
   })
+  }
+
+  
 }
 
 // update a new identified user by user id
