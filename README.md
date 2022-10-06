8649ce7f8b148e8df03fea2f9a28a16b444c772a
Kazuki Yoneyama <y.kazu_0916@icloud.com>
2022-10-06 22:29:10 +1000

    k

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Week9
+
+# yoneyamakazuki@north-10-21-13-77 crud_app % npm start       
---
+++ crud_app/unitTest/test.js
@@ -0,0 +1,16 @@
+// var assert = require("assert"); //link in assertion library
+// var app = require("../server/routes/router.js");
+// var http = require("http");
+
+// describe("Tests for server", () => {
+//   describe("Test Case 1 #fnOne()", () => {
+//     it("should return -1 when the value is not present", () => {
+//       assert.equal([1, 2, 3].indexOf(4), -1);
+//     });
+//   });
+//   describe("Test Case #fnOne()", () => {
+//     it("should return 3 as the value is present", () => {
+//       assert.equal([1, 2, 3, 4, 5].indexOf(4), 3);
+//     });
+//   });
+// });
